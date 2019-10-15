0247d0b9748bc9c640f0888bc906fd700f3c699a
amitarora.2033@gmail.com <amitarora.2033@gmail.com>
2019-10-15 21:39:17 +0530

    Fix for refactoring

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import Home from './src/screens/Home';
+import Home from './src/app/screens/Home';
 
 const navigator = createStackNavigator(
   {
